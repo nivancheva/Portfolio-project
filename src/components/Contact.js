f5f0cd5078b78cbf4ec9b126ccc9c0df674e5a7a
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-08-17 16:25:42 +0300

    added font awesome

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faGoogle, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 export default function Contact() {
     return (
         <>
             <h1>Contact</h1>
             <div>
-
+                <FontAwesomeIcon icon={faGoogle} />
+                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faLinkedin} />
             </div>
         </>
     )
